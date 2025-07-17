015b5942bf59f1a5937413c8b3f0b80233ea6ed6
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-07-17 16:45:44 +0300

    add storage for the tftstate file

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.36.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "StorageRG"
+    storage_account_name = "tsekovstorageacc"
+    container_name       = "taskboardcontainer"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
